0be3649eae7758c0f278d12184df9edda4a5acd3
Jorge Herrera <jorgehr99jhr@gmail.com>
2021-06-07 20:52:07 -0600

    Componente tarjeta y se añadio al inicio

---
+++ Cliente/src/components/Card.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <b-card
+      :title="nombre"
+      style="width: 25rem; heigth: 30rem;"
+      class="mb-2"
+      v-bind:class="estatus"
+    >
+      <b-card-text> {{ descripcion }} </b-card-text>
+      <p> {{ reportero }} </p> <p> {{ prioridad }} </p>
+      <p> {{ categoria }} </p>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    nombre: String,
+    descripcion: String,
+    prioridad: String,
+    reportero: String,
+    categoria: String,
+    estatus: String,
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    Color() {
+      return this.color;
+    },
+  },
+};
+</script>
+
+<style scoped>
+    
+</style>
--- Cliente/src/views/Home.vue
+++ Cliente/src/views/Home.vue
@@ -1,5 +1,66 @@
 <template>
-    <div>
-        <img class='mt-5' alt='Imagen de bienvenida' src='../assets/CatsWelcome.svg'  />
+  <div>
+    <img
+      class="mt-2"
+      alt="Imagen de bienvenida"
+      src="../assets/CatsWelcome.svg"
+      width="200"
+    />
+    <div class="contenedor">
+      <Card
+        v-for="ticket in tickets"
+        :key="ticket.id"
+        :nombre="ticket.nombre"
+        :descripcion="ticket.descripcion"
+        :reportero="ticket.reportero"
+        :categoria="ticket.categoria"
+        :estatus="obtenerEstatus(ticket.estatus)"
+        :prioridad="obtenerPrioridad(ticket.prioridad)"
+      />
     </div>
+  </div>
 </template>
+
+<script>
+import Card from "../components/Card";
+import { mapState, mapActions } from "vuex";
+export default {
+  name: "Home",
+  components: {
+    Card,
+  },
+  
+  computed: {
+    ...mapState(["tickets", "categorias"]),
+  },
+  methods: {
+    ...mapActions(["obtenerTickets", "obtenerCategorias"]),
+    obtenerEstatus(estatus) {
+      if (estatus === "FIN") return "border-danger";
+      if (estatus === "ESP") return "border-success";
+      if (estatus === "FIN") return "border-secondary";
+    },
+    obtenerPrioridad(prioridad) {
+      if (prioridad === "1") return "Baja";
+      if (prioridad === "2") return "Media";
+      if (prioridad === "3") return "Alta";
+    },
+  },
+  created() {
+    this.obtenerTickets();
+    this.obtenerCategorias();
+  },
+};
+</script>
+
+<style>
+.contenedor {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  flex-wrap: wrap;
+  justify-content: start;
+  gap: 1rem;
+  padding: 4rem;
+}
+</style>
